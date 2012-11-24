8c97bef9c4c020304e78d2c31a7b1964a58a87e0
Andreas Steiner <andstein@student.ethz.ch>
2012-11-24 15:34:34 +0100

    updated artwork; included logo into protoype3.v1
    also added brew compatible update.sh (commented by default)

--- hardware/geda/prototype3/update.sh
+++ hardware/geda/prototype3/update.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#gsch2pcb -f -d ../footprints -d /usr/local/Cellar/pcb/20110908/share/pcb/pcblib-newlib/ prototype3.sch
 gsch2pcb -f -d ../footprints prototype3.sch
+#gnetlist -g geda prototype3.sch -o prototype3.net
+
